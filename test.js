904901271412511bd3e3d37c78dbe09614a13549
Scott Atkins <scottrick49@gmail.com>
2016-01-23 10:53:57 -0600

    Some things.

--- test.js
+++ test.js
@@ -1,8 +1,13 @@
 var http = require('http');
 
+var testString = "<font size=\"3\" color=\"red\">hatfat?</font>";
+
 var server = http.createServer(function(req, res) {
-  res.writeHead(200);
-  res.end('Hello Http');
+	console.log("req: " + req);
+	console.log("res: " + res);
+
+	res.writeHead(200);
+	res.end("<html>DiabloData\n" + testString + "</html>");
 });
 
 server.listen(8080);
